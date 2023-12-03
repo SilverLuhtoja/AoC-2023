0556152ee00a571c73550571121a1abacfd1c6e4
Silver Luhtoja <silver.luhtoja@gmail.com>
2023-12-03 23:29:03 +0000

    noup

---
+++ day_2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type GameConfig struct {
+	red   int
+	green int
+	blue  int
+}
+
+// 2 , 6, 9, = 17
+var input string = `
+	Game 1: 1 red, 5 blue, 1 green; 16 blue, 3 red; 6 blue, 5 red; 4 red, 7 blue, 1 green
+	Game 2: 4 blue; 4 red, 3 blue, 1 green; 4 red, 9 blue, 2 green; 5 blue, 7 green, 4 red
+	Game 3: 10 blue; 7 blue, 1 green; 19 blue, 1 green, 9 red
+	Game 4: 2 green; 14 blue, 14 red, 4 green; 12 red, 11 green, 13 blue; 5 green, 9 red, 4 blue; 9 red, 7 green, 12 blue; 2 green, 3 blue, 8 red
+	Game 5: 3 blue, 4 red; 12 red, 2 green, 15 blue; 1 red, 10 blue, 1 green
+	Game 6: 1 blue, 7 red; 3 green, 5 red, 1 blue; 1 green, 7 red; 6 red, 1 blue, 4 green; 1 green, 8 red, 1 blue; 2 green, 4 red, 1 blue
+	Game 7: 11 green, 10 blue, 2 red; 1 green, 12 blue, 2 red; 9 green, 14 blue; 1 red, 19 blue, 15 green
+	Game 8: 4 green, 2 red, 14 blue; 9 green, 1 red, 15 blue; 2 green, 9 red, 8 blue; 11 green, 7 red, 8 blue; 9 red, 7 green, 6 blue
+	Game 9: 4 blue, 1 green, 2 red; 1 blue, 3 red; 1 red, 3 blue, 3 green
+	Game 10: 4 red, 3 green, 6 blue; 2 green, 15 blue, 6 red; 3 green, 2 blue; 2 red, 1 green; 11 blue, 7 red, 4 green; 2 blue, 2 red, 4 green
+`
+
+func newGameConfig(red, green, blue int) GameConfig {
+	return GameConfig{
+		red:   red,
+		green: green,
+		blue:  blue,
+	}
+}
+
+// 2274 too low, 2460 to big
+func main() {
+	rules := newGameConfig(12, 13, 14)
+	all_lines := strings.Split(Input, "Game")
+	total := 0
+	for i, line := range all_lines[1:] {
+		game := strings.Split(line, ":")
+		game_sets := strings.Split(game[1], ";")
+		isValid := rules.isValidGame(game_sets)
+
+		if isValid {
+			// fmt.Println(i + 1)
+			total += i + 1
+		}
+		// fmt.Printf("VALID : %v, GAME : %v \n", isValid, i)
+	}
+	fmt.Printf("TOTAL SUMMED WON GAMES: %v \n", total)
+}
+
+func (cfg *GameConfig) isValidGame(game_sets []string) bool {
+	for _, set := range game_sets {
+		values := strings.Split(set, ",")
+
+		for _, val := range values {
+			draw := strings.Split(val, " ")
+
+			box_count, err := strconv.Atoi(draw[1])
+			if err != nil {
+				log.Fatalf("Not a number : %s", err)
+			}
+
+			box_color := draw[2]
+
+			switch box_color {
+			case "red":
+				if box_count > cfg.red {
+					fmt.Println(box_count)
+					return false
+				}
+			case "green":
+				if box_count > cfg.green {
+					return false
+				}
+			case "blue":
+				if box_count > cfg.blue {
+					return false
+				}
+			}
+		}
+	}
+
+	// fmt.Println(game_sets)
+	return true
+}
